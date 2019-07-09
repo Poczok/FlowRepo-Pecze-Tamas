a09508ae2ed6e329d19006d9914e0913e5f18232
Poczok <tamaspece@gmail.com>
2019-07-09 16:35:20 +0200

    empty codewars project added, WIP

---
+++ Java/CodeWars/Snail/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] array
+                = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        System.out.println(method(array));
+    }
+
+    private static int[] method(int[][] array) {
+        int[] re = {};
+        while(re.length < (array.length * array.length)) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j <array.length; j++) {
+                    
+                }
+            }
+        }
+
+        return re;
+    }
+}

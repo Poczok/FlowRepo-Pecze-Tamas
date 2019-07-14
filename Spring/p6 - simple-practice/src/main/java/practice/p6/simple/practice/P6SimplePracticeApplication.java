a4cf7b419d53c63c3b86d95b46818b9cd66b4430
Poczok <tamaspece@gmail.com>
2019-07-14 18:07:50 +0200

    Spring Boot app init

---
+++ Spring/p6 - simple-practice/src/main/java/practice/p6/simple/practice/P6SimplePracticeApplication.java
@@ -0,0 +1,13 @@
+package practice.p6.simple.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P6SimplePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P6SimplePracticeApplication.class, args);
+	}
+
+}
